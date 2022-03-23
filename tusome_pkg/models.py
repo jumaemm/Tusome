20818f0485a9c02cd1d63d5b47eb0f1385fb1ae6
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-03-23 23:10:14 +0300

    Models

--- tusome_pkg/models.py
+++ tusome_pkg/models.py
@@ -1,5 +1,30 @@
 #TODO: User, Book, Review, Preferences
+from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable= False)
+    password_hash = db.Column(db.String(60), nullable=False)
+    #books = db.relationship('Book', backref='owner', lazy=True)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(400), nullable=False)
+    author = db.Column(db.String(120),nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    #TODO: COVER
+    reviews = db.relationship('Review', backref='reviewer', lazy=True)
+
+#TODO: OWNERS
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False, unique=True)
+    book_review = db.Column(db.String(2028, nullable=False))
+    #TODO: Foreign_key for reviewer, book
 
