dfebaf78aa6a7cd3753ac1426b60456e2061e1d2
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-04-06 00:04:42 +0300

    TODO: book display logic and link to db

--- tusome_pkg/site/routes.py
+++ tusome_pkg/site/routes.py
@@ -1,9 +1,16 @@
 from flask import render_template, session
 from tusome_pkg.site import bp 
 from models import Book
+import os, urllib.request, json
 
 @bp.route('/home')
 def home_page():
+    bestsellers_dict = get_bestsellers()
+    book_list = []
+    i = 0
+    #while i < 15:
+        #append to empty list a dictionary with isbn13, title, description, author, book_image
+        #book_list.append([{bestsellers_dict.books[i].isbns.isbn13: bestsellers_dict.books[i].title, }])
     return render_template('site/home.html', session = session)
 
 @bp.route('/reviews')
@@ -13,3 +20,11 @@ def review_page():
 @bp.route('/details')
 def book_details():
     return render_template('site/book_detail.html')
+
+#Make API calls to the NYTimes Books API
+def get_bestsellers():
+    url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key={}".format(os.environ.get("NYT_APIKEY"))
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return dict.results
