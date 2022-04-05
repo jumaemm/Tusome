27df74cf37f9e866633ee9b1da22e53187e7329c
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-04-05 22:59:56 +0300

    Template structure

--- tusome_pkg/site/routes.py
+++ tusome_pkg/site/routes.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import render_template, session
 from tusome_pkg.site import bp 
+from models import Book
 
 @bp.route('/home')
 def home_page():
-    return render_template('site/home.html')
+    return render_template('site/home.html', session = session)
+
+@bp.route('/reviews')
+def review_page():
+    return render_template('site/reviews.html')
+
+@bp.route('/details')
+def book_details():
+    return render_template('site/book_detail.html')
