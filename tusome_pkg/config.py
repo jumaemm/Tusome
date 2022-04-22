d04c6c6da1bb896358bb6900c9b0665dc5919e54
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-04-22 23:36:56 +0300

    Config file plus db changes

---
+++ tusome_pkg/config.py
@@ -0,0 +1,38 @@
+import os 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY='8549c630be55c1f8d724e4dc' 
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    TUSOME_MAIL_SUBJECT_PREFIX = '[Tusome]'
+    TUSOME_MAIL_SENDER = 'TUSOME Admin <tusome@example.com>'
+    TUSOME_ADMIN = os.environ.get('TUSOME_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+class DevelopmentConfig(Config):
+    DEBUG=True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, "instance//tusome-dev.sqlite")
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, "instance//tusome-test.sqlite")
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, "instance//tusome.sqlite")
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
