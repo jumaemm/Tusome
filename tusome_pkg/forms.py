289360b51708338d91c60ee29bcf8539630b857e
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-05-08 23:34:57 +0300

    Min Length of Reviews to 10 from 100

--- tusome_pkg/forms.py
+++ tusome_pkg/forms.py
@@ -36,7 +36,7 @@ class LoginForm(FlaskForm):
 
 class ReviewForm(FlaskForm):
     user = StringField(label='Username', validators=[DataRequired()])
-    review = TextAreaField(label='Review',validators=[Length(min = 100, max = 2000), DataRequired()])
+    review = TextAreaField(label='Review',validators=[Length(min = 10, max = 2000), DataRequired()])
     book_isbn = StringField(label='Book', validators=[Length(min=13)])
     submit = SubmitField(label='Submit')
 
