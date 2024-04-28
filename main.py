bf58437d66f2ca428d4603f495747684fbc6f19c
hazy254 <jumaemmanuel.EJ@outlook.com>
2024-04-28 16:35:32 +0300

    Session management

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from tusome_pkg import create_app
-from tusome_pkg import config
+from tusome_pkg.config import Config
 
 from dotenv import load_dotenv
 
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 app = create_app(os.getenv('FLASK_CONFIG'))
-
+app.permanent_session_lifetime = Config.PERMANENT_SESSION_LIFETIME
 
 if __name__ == '__main__':
     app.run(debug=True)
